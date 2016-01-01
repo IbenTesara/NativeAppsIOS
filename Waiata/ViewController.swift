7942a26737f03219ef379f777de6f17badcc15b7
Admin <Admin@mini-van-admin.lan>
2016-01-01 10:27:55 +0100

    Initial Commit

---
+++ Waiata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waiata
+//
+//  Created by Admin on 1/01/16.
+//  Copyright © 2016 Iben Van de Veire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
